9867ce7ba6713f5c77396139005efefb93902c5e
WelcomeWardy <madhavkoduvayur@gmail.com>
2026-02-23 19:57:49 +0530

    basic structure

---
+++ configs/config.py
@@ -0,0 +1,8 @@
+# configs/config.py
+
+DATASET_PATH = r"C:\Users\Madhav V K\RC\Datasets\archive\tiny-imagenet-200"
+PROCESSED_PATH = "data/processed"
+
+IMAGE_SIZE = 64
+NUM_CLASSES = 200
+IMAGES_PER_CLASS = 10  # 200 x 10 = 2000
